0b58af2df67491c895653e1cebfd0a2ac4d7603e
Joshua Brutscher <joshuabrutscher@Joshuas-MacBook-Pro.local>
2020-03-21 23:10:13 +0100

    Changed to dezimal.

--- hackatonfrontend/lib/model/Answer.dart
+++ hackatonfrontend/lib/model/Answer.dart
@@ -3,11 +3,13 @@ class Answer {
   String after;
   bool solution;
   int question;
-  int x;
-  int y;
+  double x;
+  double y;
+  double size_before;
+  double size_after;
   bool clicked;
 
-  Answer({this.before, this.after, this.solution, this.question, this.x, this.y}) {
+  Answer({this.before, this.after, this.solution, this.question, this.x, this.y, this.size_before, this.size_after}) {
     this.clicked = false;
   }
 
@@ -17,8 +19,11 @@ class Answer {
       after: json['after'],
       solution: json['solution'],
       question: json['question'],
-      x: json['x'],
-      y: json['y'],
+      x: double.parse(json['x']),
+      y: double.parse(json['y']),
+      size_before: double.parse(json['size_before']),
+      size_after: double.parse(json['size_after']),
+
     );
   }
 }
