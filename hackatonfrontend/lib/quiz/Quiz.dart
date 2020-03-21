6923fa46b0bbc5ff63f40d719366d23301d4a108
Joshua Brutscher <joshuabrutscher@Joshuas-MacBook-Pro.local>
2020-03-21 12:48:40 +0100

    Added init in Quiz

--- hackatonfrontend/lib/quiz/Quiz.dart
+++ hackatonfrontend/lib/quiz/Quiz.dart
@@ -1,6 +1,9 @@
 import "dart:developer" as console;
 import 'package:flutter/material.dart';
 import 'package:hackatonfrontend/quiz/CurvePainter.dart';
+import 'package:hackatonfrontend/model/Question.dart';
+import 'package:hackatonfrontend/services/Rest.dart';
+
 
 class Quiz extends StatelessWidget {
   @override
@@ -26,6 +29,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  Future<List<Question>> futureQuestion;
+
+  @override
+  void initState() {
+    super.initState();
+    this.futureQuestion =  Rest.instance.fetchQuestionList();
+  }
+
   @override
   Widget build(BuildContext context) {
 
